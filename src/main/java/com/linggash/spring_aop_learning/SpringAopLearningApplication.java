b923d15ff1f7f35d8edf1d97b2d83c01088e86b2
linggash <sempanalingga@gmail.com>
2024-09-02 11:47:30 +0700

    Init Project

---
+++ src/main/java/com/linggash/spring_aop_learning/SpringAopLearningApplication.java
@@ -0,0 +1,13 @@
+package com.linggash.spring_aop_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAopLearningApplication.class, args);
+	}
+
+}
