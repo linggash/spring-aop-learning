0409a78de1ec51fb2e846f5be13bd5eaac945d87
linggash <sempanalingga@gmail.com>
2024-09-04 08:26:06 +0700

    Learned about Proceeding Join Point

--- src/main/java/com/linggash/spring_aop_learning/aspect/LogAspect.java
+++ src/main/java/com/linggash/spring_aop_learning/aspect/LogAspect.java
@@ -2,6 +2,8 @@ package com.linggash.spring_aop_learning.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -20,12 +22,22 @@ public class LogAspect {
     public void beforeHelloServiceMethod(JoinPoint joinPoint){
         String className = joinPoint.getTarget().getClass().getName();
         String methodName = joinPoint.getSignature().getName();
-        log.info("Before " + className + "." + methodName + "()");
+        log.info("Before {}.{}()", className, methodName);
     }
 
-    @Before("helloServiceMethod()")
-    public void beforeHelloServiceMethod2(){
-        log.info("Before HelloService Method Again");
-    }
+    @Around("helloServiceMethod()")
+    public Object aroundHelloServiceMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+        String className = joinPoint.getTarget().getClass().getName();
+        String methodName = joinPoint.getSignature().getName();
+        try {
+            log.info("Around Before {}.{}()", className, methodName);
+            return joinPoint.proceed(joinPoint.getArgs());
+        }catch (Throwable throwable){
+            log.info("Around Error {}.{}()", className, methodName);
+            throw throwable;
+        }finally {
+            log.info("Around Finally {}.{}()", className, methodName);
 
+        }
+    }
 }
